8283ff59a23b6620c2d239f22305623a92319f89
Ubaid-ur-Rehman12 <114662372+Ubaid-ur-Rehman12@users.noreply.github.com>
2023-01-17 15:20:00 +0500

    Add files via upload

---
+++ counter.js
@@ -0,0 +1,23 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+import chalkAnimation from "chalk-animation";
+const sleep = () => {
+    return new Promise((resolve) => {
+        setTimeout(resolve, 2000);
+    });
+};
+async function welcome() {
+    let rainbow = chalkAnimation.rainbow("      <<<<<Welcome to Currency Converter>>>>>");
+    await sleep();
+    rainbow.stop();
+}
+await welcome();
+const answer = await inquirer.prompt([
+    {
+        name: "Sentence",
+        type: "input",
+        message: "Enter Your Sentence: ",
+    }
+]);
+let result = answer.Sentence.trim().split(" ");
+console.log(`Your Sentence Word Count is: ${result.length}`);
