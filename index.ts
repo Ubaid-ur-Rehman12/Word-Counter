98bce131d352eef6766cd74f5d01df6fdb897c1d
Ubaid-ur-Rehman12 <114662372+Ubaid-ur-Rehman12@users.noreply.github.com>
2023-01-25 14:04:53 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,26 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+import chalkAnimation from "chalk-animation";
+const sleep = ()=>{
+    return new Promise((resolve) => {
+        setTimeout(resolve,2000);
+    })
+}
+async function welcome() {
+    let rainbow = chalkAnimation.rainbow("      <<<<<Welcome to Word Counter>>>>>")
+    await sleep();
+    rainbow.stop()
+}
+await welcome();
+
+const answer:{
+    Sentence : string,
+} =await inquirer.prompt([
+    {
+        name : "Sentence",
+        type : "input",
+        message : "Enter Your Sentence: ",
+   }
+])
+let result = answer.Sentence.trim().split(" ");
+console.log(`Your Sentence Word Count is: ${result.length}`);
